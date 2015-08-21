592174208e886b875dec0c8b8cdff869287283e8
Sarah Quigley <sarah@sarahquigley.net>
2015-08-21 12:04:19 +0100

    Build updated code.

--- build/main.js
+++ build/main.js
@@ -1,4 +1,20 @@
 (function() {
+  var MockElement,
+    bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
+  MockElement = (function() {
+    function MockElement() {
+      this.addEventListener = bind(this.addEventListener, this);
+      this.clientWidth = 0;
+      this.clientHeight = 0;
+    }
+
+    MockElement.prototype.addEventListener = function() {};
+
+    return MockElement;
+
+  })();
+
+  window.MockElement = MockElement;
 
 }).call(this);
